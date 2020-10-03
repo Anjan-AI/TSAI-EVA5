6a1acffc7fbf3dec2bfbfefa36499fe8875fdb09
Anjan-AI <anjan.student@gmail.com>
2020-10-03 12:37:12 +0530

    updated codes

--- Session 10 - Advanced Concepts in Training & Learning Rates/Utils/evaluate.py
+++ Session 10 - Advanced Concepts in Training & Learning Rates/Utils/evaluate.py
@@ -10,7 +10,7 @@ import torch
 import torchvision
 
 
-def show_misclassified_images(model, device, dataset, classes,no_images):
+def show_misclassified_images(model, device, dataset, classes,number = 25):
   misclassified_images = []
   
   for images, labels in dataset:
@@ -18,24 +18,26 @@ def show_misclassified_images(model, device, dataset, classes,no_images):
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             for i in range(len(labels)):
-              if(len(misclassified_images)<no_images and predicted[i]!=labels[i]):
+              if(len(misclassified_images)<number and predicted[i]!=labels[i]):
                 misclassified_images.append([images[i],predicted[i],labels[i]])
-            if(len(misclassified_images)>no_images):
+            if(len(misclassified_images)>number):
               break
-    
-  
+  return misclassified_images
+
+def plot_misclassified_images(misclassified_images, classes,number = 25) :
+
   fig = plt.figure(figsize = (8,8))
-  for i in range(no_images):
+  for i in range(25):
         sub = fig.add_subplot(5, 5, i+1)
-        #imshow(misclassified_images[i][0].cpu())
         img = misclassified_images[i][0].cpu()
-        img = img / 2 + 0.5 
+        img = img *2  + 0.5 
         npimg = img.numpy()
         plt.imshow(np.transpose(npimg,(1, 2, 0)),interpolation='none')
-        
+
         sub.set_title("P={}, A={}".format(str(classes[misclassified_images[i][1].data.cpu().numpy()]),str(classes[misclassified_images[i][2].data.cpu().numpy()])))
-        
+        sub.axis('off')
   plt.tight_layout()
+
   
   
 def evaluate_classwise_accuracy(model, device, classes, test_loader):
