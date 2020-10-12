e46a9946dc1eaf2e2ea79e718422a1f9772b2687
Anjan-AI <anjan.student@gmail.com>
2020-10-12 13:09:57 +0530

    session 11

--- Session 11 - Super Convergence/test_code.py
+++ Session 11 - Super Convergence/test_code.py
@@ -9,24 +9,36 @@ import os
 import torchvision
 import torchvision.transforms as transfroms
 from Dataset.dataset import Dataset,cifar10_classes
-from Dataset.image_augmentations import AlbumentationTransforms
+#from Dataset.image_augmentations import AlbumentationTransforms
+from Dataset.image_aug import AlbumentationTransforms
 from Utils.SetCuda import set_seed,initialize_cuda
 from Utils.utilities import print_model_summary,cross_entropy_loss
 from Utils.evaluate import show_misclassified_images,evaluate_classwise_accuracy,plot_misclassified_images
 from Utils.GradCam import GradCamView
 from Utils.show_image import imshow ,show_train_data
-from models.resnet import ResNet18
+#from models.resnet import ResNet18
+from models.S11_model import NewResnet
 from Training.Training_Testing import train ,test,runmodel
 from Utils.plot import plot_metric
 import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
 
 from Utils.lr_finder import LRFinder
-
+import albumentations as A
+from Utils.CyclicLRtest import LRRangeFinder
 classes = cifar10_classes()
 
-train_transform = AlbumentationTransforms(augmentation = True, mean=(0.4914, 0.4822, 0.4465) ,std_dev =(0.2471, 0.2435, 0.2616),rotate_degree =12.0,cutout= True)
-test_transform = AlbumentationTransforms()
+channel_means = (0.4914, 0.4822, 0.4465)
+channel_stdevs = (0.2471, 0.2435, 0.2616)
+train_transform = AlbumentationTransforms([
+                                       A.Rotate((-30.0, 30.0)),
+                                       A.HorizontalFlip(),
+                                       A.RGBShift(r_shift_limit=20, g_shift_limit=20, b_shift_limit=20, p=0.5),
+                                       A.Normalize(mean=channel_means, std=channel_stdevs),
+                                       A.Cutout(num_holes=2, max_h_size=9,max_w_size = 9,p=1) 
+                                       ])
+test_transform = AlbumentationTransforms([A.Normalize(mean=channel_means, std=channel_stdevs)])
+
 
 Data = Dataset(train_transform,test_transform)
 
@@ -45,7 +57,7 @@ test_loader = Data.data_loader(test_set,cuda= cuda ,batch_size = 32 , num_worker
 
 show_train_data(train_loader, classes)
 
-model = ResNet18()
+model = NewResnet()
 print_model_summary(model, input_size = (3,32,32),device =device)
 
 
